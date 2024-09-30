db2f31d148605ffe51fb4684fda662d6c93b4dae
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-29 23:14:58 -0400

    Added Player movement Player can move with WASD or arrow keys in a normalized direction

--- scipts/player.gd
+++ scipts/player.gd
@@ -1,1 +1,24 @@
 extends CharacterBody2D
+
+@export var move_speed := 300
+
+# Player has an attribute "velocity" which move_and_slide() uses to move
+func _physics_process(delta: float) -> void:
+	# Only move when the keys are actually pressed
+	velocity = Vector2.ZERO
+	
+	# Capture the desiried directional movement
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	
+	# Make sure your vector is normalized
+	velocity = velocity.normalized() * move_speed
+	
+	# Call this to move the player by its velocity
+	move_and_slide()
