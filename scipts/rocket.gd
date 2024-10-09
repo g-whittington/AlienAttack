e64688ea3d5a86cf072a6834d5bb5431f4400474
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-10-09 00:28:47 -0400

    Finished learning about delta delta is the basically the time from the last frame with 60fps, delta will be 1/60=0.01666667 This makes it so a movement speed var will turn to the total amount of pixels / second you want the object to move that is frame rate independent

--- scipts/rocket.gd
+++ scipts/rocket.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-@export var speed: int = 375
+@export var speed: int = 300
 
 func _physics_process(delta: float) -> void:
+	# Having speed*delta, it now changes the pixels/second moved to be
+	# what we set the speed variable to. 
 	global_position.x += speed*delta
