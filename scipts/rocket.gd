c3b0d19dec5d5bd801a83acbe94a77ed2a9e0fac
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-10-08 22:29:54 -0400

    Rocket speed not multiplied by delta

--- scipts/rocket.gd
+++ scipts/rocket.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed: int = 8
+@export var speed: int = 375
 
 func _physics_process(delta: float) -> void:
-	global_position.x += speed
+	global_position.x += speed*delta
