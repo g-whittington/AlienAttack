d2c60bfc5436ec153b0fe6fb8387f58ed470f2db
g-whittington <105523254+g-whittington@users.noreply.github.com>
2025-01-12 22:36:23 -0500

    Made so the each rocket instance is destroyed when off screen Done by using the queue_free() function Signals can also be connected through code in the _ready method

--- scipts/rocket.gd
+++ scipts/rocket.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
 @export var speed: int = 500
+# used to delete rocket instances when the rocket is off screen
+@onready var visible_notifier: VisibleOnScreenNotifier2D = $VisibleNotifier
+
+func _ready():
+	# can use code to connect signals
+	visible_notifier.connect("screen_exited", _on_screen_exited)
 
 func _physics_process(delta: float) -> void:
 	# Having speed*delta, it now changes the pixels/second moved to be
 	# what we set the speed variable to. 
 	global_position.x += speed*delta
+
+func _on_screen_exited():
+	# deletes the instance at the end of the frame it is called on
+	queue_free()
