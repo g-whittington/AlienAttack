b7c944a922494e05fe31621b15a713a253c3dc6b
g-whittington <105523254+g-whittington@users.noreply.github.com>
2025-01-12 23:34:35 -0500

    Added the spawning functionality to EnemySpawner

--- scipts/enemy_spawner.gd
+++ scipts/enemy_spawner.gd
@@ -5,6 +5,17 @@ extends Node2D
 const ENEMY = preload("res://scenes/enemy.tscn")
 
 @onready var timer: Timer = $Timer
+@onready var spawn_positions: Node2D = $SpawnPositions
 
 func _on_timer_timeout() -> void:
-	print("one seonds")
+	spawn_enemy()
+	
+func spawn_enemy() -> void:
+	# create an enemy
+	var enemy_instance := ENEMY.instantiate()
+	# get one of the SpawnPos's location
+	var spawn_position : Marker2D = spawn_positions.get_children().pick_random()
+	# change the enemy postion to be on the marker
+	enemy_instance.global_position = spawn_position.global_position
+		
+	add_child(enemy_instance)
